9e797f86dec8b825cc7ab2826072fdbff6acffb4
MatheusZowysky <MatheusML_06@hotmail.com>
2021-04-13 14:31:24 -0300

    commitao

---
+++ src/main/java/br/theus/zowysky/AulaSts/AulaStsApplication.java
@@ -0,0 +1,13 @@
+package br.theus.zowysky.AulaSts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaStsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/theus/zowysky/AulaSts/AulaStsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.theus.zowysky.AulaSts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
